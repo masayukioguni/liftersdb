0aa43de9ad93ad3682b3337773cf25564403a903
masayuki oguni <masayuki.oguni.dev@gmail.com>
2012-07-05 01:18:20 +0900

    ペジネーション実験

--- spec/factories/lifters.rb
+++ spec/factories/lifters.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :lifter do
     name { Faker::Name.name }
-    gender { Random.number(1) }
+    gender { Random.number(2) }
     birthday { Random.date(1000) }
   end
 end
